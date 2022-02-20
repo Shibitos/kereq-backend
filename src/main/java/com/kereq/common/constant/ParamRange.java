f8dc115fb58ea1d0ff0fbac66cd0ada9f1926e2f
unknown <ashibito@gmail.com>
2022-02-20 12:18:38 +0100

    Environment params range

---
+++ src/main/java/com/kereq/common/constant/ParamRange.java
@@ -0,0 +1,21 @@
+package com.kereq.common.constant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum ParamRange {
+
+    IMG_DIR_DIV_CHAR_COUNT(ParamKey.IMG_DIR_DIV_CHAR_COUNT, 1, 32),
+    IMG_DIR_DIV_MAX_LEVEL(ParamKey.IMG_DIR_DIV_MAX_LEVEL, 1, 0),
+    IMG_MIN_SIZE(ParamKey.IMG_MIN_SIZE, 50, 50),
+    PHOTO_MAX_WIDTH(ParamKey.PHOTO_MAX_WIDTH, 50, 5000),
+    PHOTO_MAX_HEIGHT(ParamKey.PHOTO_MAX_HEIGHT, 50, 5000),
+    PHOTO_PROFILE_MAX_WIDTH(ParamKey.PHOTO_PROFILE_MAX_WIDTH, 50, 5000),
+    PHOTO_PROFILE_MAX_HEIGHT(ParamKey.PHOTO_PROFILE_MAX_HEIGHT, 50, 5000);
+
+    String key;
+    double min;
+    double max;
+}
--- src/main/java/com/kereq/common/service/EnvironmentService.java
+++ src/main/java/com/kereq/common/service/EnvironmentService.java
@@ -1,6 +1,8 @@
 package com.kereq.common.service;
 
 import com.kereq.common.constant.CacheName;
+import com.kereq.common.constant.ParamKey;
+import com.kereq.common.constant.ParamRange;
 import com.kereq.common.error.CommonError;
 import com.kereq.main.exception.ApplicationException;
 import org.springframework.cache.annotation.Cacheable;
@@ -9,6 +11,8 @@ import org.springframework.core.env.Environment;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+
 @Service
 public class EnvironmentService implements EnvironmentAware {
 
@@ -16,19 +20,31 @@ public class EnvironmentService implements EnvironmentAware {
 
     @Cacheable(value = CacheName.PARAMS, key = "#key")
     public String getParam(String key) {
-        String value = environment.getProperty(key);
-        if (value == null) {
+        return getEnvProperty(key);
+    }
+
+    @Cacheable(value = CacheName.PARAMS, key = "#key")
+    public Integer getParamInteger(String key) {
+        int value = Integer.parseInt(getEnvProperty(key));
+        boolean rangeConditionFailed = Arrays.stream(ParamRange.values())
+                .anyMatch(pr -> pr.getKey().equals(key)
+                        && ((pr.getMin() > 0 && value < pr.getMin()) || (pr.getMax() > 0 && value > pr.getMax())));
+        if (rangeConditionFailed) {
             throw new ApplicationException(CommonError.OTHER_ERROR);
         }
         return value;
     }
 
-    public Integer getParamInteger(String key) {
-        return Integer.valueOf(getParam(key));
-    }
-
     @Override
     public void setEnvironment(@NonNull Environment environment) {
         this.environment = environment;
+    }
+
+    private String getEnvProperty(String key) {
+        String value = environment.getProperty(key);
+        if (value == null) {
+            throw new ApplicationException(CommonError.OTHER_ERROR);
+        }
+        return value;
     }
 }
